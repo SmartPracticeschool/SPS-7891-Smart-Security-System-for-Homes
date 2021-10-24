3c2770a42fac70c85e25a9c5f63a9d386ffea34f
amino19 <anshumaankrprasad76@gmail.com>
2021-10-24 18:00:02 +0530

    ✨ modified project

--- test.py
+++ test.py
@@ -1,32 +1,30 @@
 import cv2
-import numpy as np
-import time
-face_classifier=cv2.CascadeClassifier("haar-face.xml")
+# import numpy as np
+# import time
+face_classifier = cv2.CascadeClassifier("haar-cascade-frontal-face.xml")
 
-#It will read the first frame/image of the video
-video=cv2.VideoCapture(0)
+# It will read the first frame/image of the video
+video = cv2.VideoCapture(0)
 
 while True:
-    #capture the first frame
-    check,frame=video.read()
-    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # capture the first frame
+    check, frame = video.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #detect the faces from the video using detectMultiScale function
-    faces=face_classifier.detectMultiScale(gray,1.3,5)
+    # detect the faces from the video using detectMultiScale function
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
     
-    #drawing rectangle boundries for the detected face
-    for(x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (127,0,255), 2)
+    # drawing rectangle boundaries for the detected face
+    for(x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (127, 0, 255), 2)
         cv2.imshow('Face detection', frame)
-        cv2.imwrite('pic.jpg',frame)
-        
-        
+        cv2.imwrite('pic.jpg', frame)
 
-    #waitKey(1)- for every 1 millisecond new frame will be captured
-    Key=cv2.waitKey(1)
-    if Key==ord('q'):
-        #release the camera
+    # waitKey(1) - for every 1 millisecond new frame will be captured
+    Key = cv2.waitKey(1)
+    if Key == ord('q'):
+        # release the camera
         video.release()
-        #destroy all windows
+        # destroy all windows
         cv2.destroyAllWindows()
         break
